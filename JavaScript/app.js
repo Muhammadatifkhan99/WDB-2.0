d71fb59e5bb12a6b6fecb6e37a5ba2213c51d35a
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2024-08-03 09:08:03 +0500

    JS Conditionals

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,3 +1,22 @@
-console.log("Hello from the javascript")
+// let random = Math.random();
 
-total = 1+3;
+// if(random < 0.5){
+//     console.log("The random number is less than 0.5")
+//     console.log(random)
+// }
+// if(random > 0.5){
+//     console.log("The random number is greater than 0.5")
+//     console.log(random);
+// }
+
+// console.log("These are everthing about the JS if Statements")
+
+const dayOfWeek = "Monday"
+
+if (dayOfWeek === "Friday"){
+    console.log("Yay its friday")
+} else if (dayOfWeek === "Monday"){
+    console.log("its again Monday")
+} else if (dayOfWeek === "Saturday"){
+    console.log("I love Saturdays")
+}
