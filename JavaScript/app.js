0c43e7474ccd8953dbf070e8383ad391ff5da076
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2024-08-03 11:17:38 +0500

    IF else and nested if else statements

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -22,3 +22,23 @@
 // }
 
 
+//password must be greater than 6 characters
+//password should not contain spaces
+
+const password = prompt("Enter a valid password");
+
+if (password.length >=6){
+    if(password.indexOf(' ') === -1){
+        console.log("Valid Passcode")
+    } else {
+        console.log("No spaces allowed in password")
+    }
+} else {
+    console.log("Password Less than 6 characters")
+}
+
+// if(password.indexOf(' ') === -1){
+//     console.log("Good Job no spaces in password")
+// } else {
+//     console.log("No spaces allowed in password")
+// }
