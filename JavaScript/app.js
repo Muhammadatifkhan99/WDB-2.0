5692797dc56c7f0533d1aad8d4a8ac1a4dbaaf1b
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2024-08-04 22:03:43 +0500

    Repeating stuff with JS, if statements switch statements and same

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -25,20 +25,38 @@
 //password must be greater than 6 characters
 //password should not contain spaces
 
-const password = prompt("Enter a valid password");
+// const password = prompt("Enter a valid password");
 
-if (password.length >=6){
-    if(password.indexOf(' ') === -1){
-        console.log("Valid Passcode")
-    } else {
-        console.log("No spaces allowed in password")
-    }
-} else {
-    console.log("Password Less than 6 characters")
-}
+// if (password.length >=6){
+//     if(password.indexOf(' ') === -1){
+//         console.log("Valid Passcode")
+//     } else {
+//         console.log("No spaces allowed in password")
+//     }
+// } else {
+//     console.log("Password Less than 6 characters")
+// }
 
 // if(password.indexOf(' ') === -1){
 //     console.log("Good Job no spaces in password")
 // } else {
 //     console.log("No spaces allowed in password")
 // }
+
+
+//Truthy and Falsy
+
+let userInput = prompt("Enter anything: ");
+
+// if (userInput){
+//     console.log("Truthy")
+// } else {
+//     console.log("Falsy")
+// }
+
+
+if(0){
+    console.log("Falsy")
+} else {
+    console.log("Truthy")
+}
