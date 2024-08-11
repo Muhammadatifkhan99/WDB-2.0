951c0c6227313661ea258f9fe4ed0c3247af7059
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2024-08-11 14:31:07 +0500

    Arrays in JS start with a refresher for it

---
+++ JavaScript Arrays/app.js
@@ -0,0 +1,16 @@
+// JavaScript arrays...store multiple values at a Time
+//Arrays are ordered collection of items,,,,collections of songs in the playlist, colloection 
+//of levels in the game,,,,collection of comments on IG post......etc etc.....
+
+
+
+let daysOfWeek = ["Mondy", "Tuesday","Wednesday"];
+console.log(daysOfWeek[0])
+console.log(daysOfWeek[1])
+console.log(daysOfWeek[2])
+
+// JS arrays does not to be homogenius we can put random stuff such as string 
+//numbers, booleans etc.....
+
+
+let kitchenSink = [true,undefined,12,9.999999,NaN,false,null,"HIIII!!!!!"]
