4f68e63cb0b0321d576ff808880d34a41b25d0a5
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2024-08-14 16:24:23 +0500

    modifying object literals in Javascript

--- JavaScript Object Literals/app.js
+++ JavaScript Object Literals/app.js
@@ -1,38 +1,68 @@
-console.log(typeof []);
+// console.log(typeof []);
 
-const person = {firstName: 'Muhammad', lastName:'Atif'};
+// const person = {firstName: 'Muhammad', lastName:'Atif'};
 
-console.log(person);
+// console.log(person);
 
-let kitchenSink = {
-    favNumber: 454545,
-    isHappy: true,
-    color: ['red','yellow', 'blue']
-}
+// let kitchenSink = {
+//     favNumber: 454545,
+//     isHappy: true,
+//     color: ['red','yellow', 'blue']
+// }
 
-console.log(kitchenSink);
-
-
-
-console.log("Accessing Values out of an Object");
-
-console.log(person["firstName"]);
-console.log(person["lastName"]);
-console.log(kitchenSink["color"]);
-console.log(kitchenSink["isHappy"]);
-
-console.log(person.firstName);
-console.log(kitchenSink.color);
+// console.log(kitchenSink);
 
 
-const years = {1999: "Good", 2020:"Bad"};
 
-console.log(years[1999]);
-console.log(years["1999"]);
+// console.log("Accessing Values out of an Object");
 
-let birthyear = 2020;
+// console.log(person["firstName"]);
+// console.log(person["lastName"]);
+// console.log(kitchenSink["color"]);
+// console.log(kitchenSink["isHappy"]);
 
-console.log(years.birthyear);
-console.log(years[birthyear]);
+// console.log(person.firstName);
+// console.log(kitchenSink.color);
+
+
+// const years = {1999: "Good", 2020:"Bad"};
+
+// console.log(years[1999]);
+// console.log(years["1999"]);
+
+// let birthyear = 2020;
+
+// console.log(years.birthyear);
+// console.log(years[birthyear]);
 
 //Anythings in the square brackets can be evaulated...
+
+
+
+const midterms = {ali: 45, khan: 44};
+console.log(midterms);
+
+console.log(midterms.ali);
+console.log(midterms.khan);
+
+
+console.log(midterms.khan = 40);
+
+console.log(midterms.khan);
+console.log(midterms);
+
+console.log(midterms.ali = "A");
+console.log(midterms.khan = "B");
+console.log(midterms.ezra = "C+");
+console.log(midterms.antonio = "B+");
+
+console.log(midterms);
+
+
+
+
+
+
+
+
+
