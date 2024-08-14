46467452d2427da5220379ce495fb0a2eaad3efc
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2024-08-14 15:15:04 +0500

    creating object literals and mixing other objects inside

--- JavaScript Object Literals/app.js
+++ JavaScript Object Literals/app.js
@@ -1,1 +1,13 @@
-console.log("hello");
+console.log(typeof []);
+
+const person = {firstName: 'Muhammad', lastName:'Atif'};
+
+console.log(person);
+
+let kitchenSink = {
+    favNumber: 454545,
+    isHappy: true,
+    color: ['red','yellow', 'blue']
+}
+
+console.log(kitchenSink);
