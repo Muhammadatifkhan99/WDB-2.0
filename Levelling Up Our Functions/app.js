fde1843a504ce3944b78320c792c5a3f4d30dd82
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2024-08-17 23:16:45 +0500

    scope of let and var in JS

--- Levelling Up Our Functions/app.js
+++ Levelling Up Our Functions/app.js
@@ -14,7 +14,28 @@
 // birdWatch();
 
 
+// let radius = 8;
 
+// if(radius > 0){
+//     const PI = 3.1459;
+//     let circ = 15;
+// }
+
+// console.log(radius);
+// console.log(PI); // Block scoping with let can be avoided with """""var"""""
+
+
+let radius = 8;
+
+if(radius > 0){
+    var PI = 3.1459;
+    var circ = 15;
+}
+//This is only possible in case of var definition but var is almost deprecated and no longer needed to be used..
+
+console.log(radius);
+console.log(circ);
+console.log(PI);
 
 
 
