aacd4397ef7bb1d3d1bbdfccb5b7fe13f2f9804e
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2024-08-17 16:21:05 +0500

    Excercises on Functions in JS

--- NEW Introducing Functions/app.js
+++ NEW Introducing Functions/app.js
@@ -28,40 +28,88 @@
 //     console.log(message.toUpperCase());
 // }
 
-function greet(firstName, lastName){
-    console.log(`Hey there, ${firstName} ${lastName[0]}.`)
+// function greet(firstName, lastName){
+//     console.log(`Hey there, ${firstName} ${lastName[0]}.`)
+// }
+
+// greet("Muhammad","Atif");
+// greet("Khan","Muhammad");
+// greet("Sahal", "Farhan");
+
+
+// function repeat(str, numTimes){
+//     let result = "";
+//     for(let i=0;i<numTimes;i++){
+//         result += str;
+//     }
+//     console.log(result)
+// }
+
+// repeat("Atif", 4);
+// repeat("A", 2);
+// repeat("f", 5);
+
+// //Snake Eye Excercise
+
+// function isSnakeEye(die1,die2){
+//     if(die1 === 1 && die2 ===1){
+//         console.log("Snake Eye");
+//     } else {
+//         console.log("Not Snake Eyes");
+//     }
+// }
+
+// isSnakeEye(1,1);
+// isSnakeEye(2,3);
+// isSnakeEye(5,4);
+
+
+//The Return Key Word
+
+//Without the return statement we cannot store values in a  variable, only we can print them
+// function add(x,y){
+//     console.log(x+y);
+// }
+
+
+///The RETURN KEY word returns only one object, we can return multiple things in an array
+///RETURN stops the execution of that function
+// function add(x,y){
+//     if(typeof x !== "number" || typeof y !== "number"){
+//         return false
+//     }
+//     let z = x + y;
+//     return z;
+// }
+
+
+function multiply(x,y){
+    return x*y;
 }
 
-greet("Muhammad","Atif");
-greet("Khan","Muhammad");
-greet("Sahal", "Farhan");
 
-
-function repeat(str, numTimes){
-    let result = "";
-    for(let i=0;i<numTimes;i++){
-        result += str;
-    }
-    console.log(result)
-}
-
-repeat("Atif", 4);
-repeat("A", 2);
-repeat("f", 5);
-
-//Snake Eye Excercise
-
-function isSnakeEye(die1,die2){
-    if(die1 === 1 && die2 ===1){
-        console.log("Snake Eye");
+function isShortsWeather(temp){
+    if(temp >= 75){
+        return true;
     } else {
-        console.log("Not Snake Eyes");
+        return false;
     }
 }
 
-isSnakeEye(1,1);
-isSnakeEye(2,3);
-isSnakeEye(5,4);
+function lastElement(arr){
+    if(!arr.length) return null;
+    return arr[arr.length-1];
+}
 
+function capitalize(str) {
+    let capital = str[0].toUpperCase() + str.slice(1);
+    return capital;
+}
 
-
+function sumOfArray(arr1){
+    let sum = 0;
+    for(a of arr1){
+        sum += a;
+    }
+    return sum;
+}
