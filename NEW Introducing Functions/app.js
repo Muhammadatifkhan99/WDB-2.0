5390b7fad736ab85adefc33242a0fb8e910b5c91
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2024-08-17 15:01:32 +0500

    Arguments to functions

--- NEW Introducing Functions/app.js
+++ NEW Introducing Functions/app.js
@@ -3,3 +3,19 @@ function sing(){
     console.log("RE");
     console.log("Do");
 }
+sing();
+sing();
+sing();
+sing();
+sing();
+sing();
+
+
+function greet(firstName){
+    console.log(`Hey there, ${firstName}`)
+}
+
+greet("Atif");
+greet("Muhammad");
+greet("Sahal");
+greet();
