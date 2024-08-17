fb7b8be0b47d1d00a96785c1135c8308b67f51e6
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2024-08-17 15:07:03 +0500

    Function Excercise

--- NEW Introducing Functions/app.js
+++ NEW Introducing Functions/app.js
@@ -1,21 +1,29 @@
-function sing(){
-    console.log("Do");
-    console.log("RE");
-    console.log("Do");
+// function sing(){
+//     console.log("Do");
+//     console.log("RE");
+//     console.log("Do");
+// }
+// sing();
+// sing();
+// sing();
+// sing();
+// sing();
+// sing();
+
+
+// function greet(firstName){
+//     console.log(`Hey there, ${firstName}`)
+// }
+
+// greet("Atif");
+// greet("Muhammad");
+// greet("Sahal");
+// greet();
+
+//Excercise
+
+function rant(message){
+    console.log(message.toUpperCase());
+    console.log(message.toUpperCase());
+    console.log(message.toUpperCase());
 }
-sing();
-sing();
-sing();
-sing();
-sing();
-sing();
-
-
-function greet(firstName){
-    console.log(`Hey there, ${firstName}`)
-}
-
-greet("Atif");
-greet("Muhammad");
-greet("Sahal");
-greet();
