167aba5634cacd030130d53290cb29544b06a866
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2024-08-17 23:05:14 +0500

    Function Scope

---
+++ Levelling Up Our Functions/app.js
@@ -0,0 +1,44 @@
+
+
+// function collectEggs(){
+//     let totalEggs = 6;
+// }
+// console.log(totalEggs)
+//Function scope is the visability of the variable out of certain block or function
+// let bird = "chenchra";
+// function birdWatch (){
+//     // let bird = "mandirin duck";
+//     console.log(bird)
+// }
+ 
+// birdWatch();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
