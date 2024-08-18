957c8d0efec236c55014e87338e46b08cadec059
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2024-08-18 17:25:29 +0500

    Extract first names from array using map on array

--- Callbacks-Array Methods/app.js
+++ Callbacks-Array Methods/app.js
@@ -63,6 +63,10 @@ let scores = movies.map(function(t){
     return t.score;
 })
 
+const fullNames = [{first: 'Albus', last: 'Dumbledore'}, {first: 'Harry', last: 'Potter'}, {first: 'Hermione', last: 'Granger'}, {first: 'Ron', last: 'Weasley'}, {first: 'Rubeus', last: 'Hagrid'}, {first: 'Minerva', last: 'McGonagall'}, {first: 'Severus', last: 'Snape'}];
+const firstNames = fullNames.map(function (f){
+    return f.first;
+})
 
 
 
