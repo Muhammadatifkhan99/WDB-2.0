7c22a5297177aa0b892d2a57465c39252a67b831
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2024-08-18 17:36:57 +0500

    Arrow functions in JavaScript

--- Callbacks-Array Methods/app.js
+++ Callbacks-Array Methods/app.js
@@ -29,45 +29,63 @@ const number = ["1","2","3","4","5","6","7","8","9","10"];
 
 ///////////////////////////////////////MAP/////////////////////////////////////
 
-const doubles = number.map(function (num){
-    return num * 2;
-})
+// const doubles = number.map(function (num){
+//     return num * 2;
+// })
 
 
-let movies = [
-    {
-        title: "Amadeus",
-        score: 99
-    },
-    {
-        title: "Parasite",
-        score: 98
-    },
-    {
-        title: "Stand by Me",
-        score: 85
-    },
-    {
-        title: "Alien",
-        score: 90
-    }
+// let movies = [
+//     {
+//         title: "Amadeus",
+//         score: 99
+//     },
+//     {
+//         title: "Parasite",
+//         score: 98
+//     },
+//     {
+//         title: "Stand by Me",
+//         score: 85
+//     },
+//     {
+//         title: "Alien",
+//         score: 90
+//     }
 
-]
+// ]
 
-let titles = movies.map(function (movie){
-    return movie.title.toUpperCase();
-})
-
-
-let scores = movies.map(function(t){
-    return t.score;
-})
-
-const fullNames = [{first: 'Albus', last: 'Dumbledore'}, {first: 'Harry', last: 'Potter'}, {first: 'Hermione', last: 'Granger'}, {first: 'Ron', last: 'Weasley'}, {first: 'Rubeus', last: 'Hagrid'}, {first: 'Minerva', last: 'McGonagall'}, {first: 'Severus', last: 'Snape'}];
-const firstNames = fullNames.map(function (f){
-    return f.first;
-})
+// let titles = movies.map(function (movie){
+//     return movie.title.toUpperCase();
+// })
 
 
+// let scores = movies.map(function(t){
+//     return t.score;
+// })
+
+// const fullNames = [{first: 'Albus', last: 'Dumbledore'}, {first: 'Harry', last: 'Potter'}, {first: 'Hermione', last: 'Granger'}, {first: 'Ron', last: 'Weasley'}, {first: 'Rubeus', last: 'Hagrid'}, {first: 'Minerva', last: 'McGonagall'}, {first: 'Severus', last: 'Snape'}];
+// const firstNames = fullNames.map(function (f){
+//     return f.first;
+// })
+
+///////////////////////////////////////////////////////////////////////////////
 
 
+///////////////////////////////////////Arrow Functions/////////////////////////
+
+
+function add(x){
+    return x+x;
+}
+
+let add1 = x => {
+    return x + x;
+}
+
+let mul = (x,y) => {
+    return x * y;
+}
+
+let nothing = () => {
+    console.log("nothing");
+}
