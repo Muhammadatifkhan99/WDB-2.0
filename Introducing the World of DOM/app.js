436bf3bf2471b804b63705111530e1906db9611b
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2024-08-20 23:50:36 +0500

    getelementsbytagname and getelementsbyclassname both returns an HTMLCollections and it had Elements inside it, while getelementbyid returns that object

--- Introducing the World of DOM/app.js
+++ Introducing the World of DOM/app.js
@@ -13,9 +13,32 @@
 
 //console.dir(document); //shows the details of the document object....
 
-const banner = document.getElementById("banner"); //This gives the object representation of that object
-console.dir(banner);
+// const banner = document.getElementById("banner"); //This gives the object representation of that object
+// console.dir(banner);
 
 
-const toc = document.getElementById("toc");
-console.dir(toc);
+// const toc = document.getElementById("toc");
+// console.dir(toc);
+
+
+//getElementsByTagName
+
+// const allImages = document.getElementsByTagName("img");
+ 
+// for(let img of allImages){
+//     img.src = "https://images.unsplash.com/photo-1445953993297-28dff00088f3?q=80&w=1812&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+// }
+
+
+//getElementsByTagName returns an HTMLCollection...and inside it have the Element, and its that object that we are getting back
+//that has all the properties that represents a single html element
+
+
+
+
+//getElementsByClassName
+
+const squareImages = document.getElementsByClassName('square');
+for(let img of squareImages){
+    img.src = "https://images.unsplash.com/photo-1445953993297-28dff00088f3?q=80&w=1812&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+}
