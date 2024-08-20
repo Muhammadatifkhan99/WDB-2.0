8f42226383e6c3199f1a6e74a32575c417cba569
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2024-08-20 23:26:48 +0500

    getElementbyid(), the world of DOM

---
+++ Introducing the World of DOM/app.js
@@ -0,0 +1,21 @@
+// const allLinks = document.querySelectorAll('a');
+
+// // for (let link of allLinks) {
+// //     link.innerText = 'I AM A LINK!!!!'
+// // }
+
+
+// for (let link of allLinks) {
+//     link.style.color = 'rgb(0, 108, 134)';
+//     link.style.textDecorationColor = 'magenta';
+//     link.style.textDecorationStyle = 'wavy'
+// }
+
+//console.dir(document); //shows the details of the document object....
+
+const banner = document.getElementById("banner"); //This gives the object representation of that object
+console.dir(banner);
+
+
+const toc = document.getElementById("toc");
+console.dir(toc);
