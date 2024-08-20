1baa2a4963fa85c15972df5080470c54fc48fb23
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2024-08-20 23:55:59 +0500

    query selectors

--- Introducing the World of DOM/app.js
+++ Introducing the World of DOM/app.js
@@ -38,7 +38,17 @@
 
 //getElementsByClassName
 
-const squareImages = document.getElementsByClassName('square');
-for(let img of squareImages){
-    img.src = "https://images.unsplash.com/photo-1445953993297-28dff00088f3?q=80&w=1812&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-}
+// const squareImages = document.getElementsByClassName('square');
+// for(let img of squareImages){
+//     img.src = "https://images.unsplash.com/photo-1445953993297-28dff00088f3?q=80&w=1812&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+// }
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+//                  querySelectors
+//
+//
+
+
+
