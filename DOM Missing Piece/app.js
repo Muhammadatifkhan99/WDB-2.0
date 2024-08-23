ba91d5ea45a1e52ae521b5d99905d853c8c40710
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2024-08-23 22:59:33 +0500

    button display the rgb value of that particular color

--- DOM Missing Piece/app.js
+++ DOM Missing Piece/app.js
@@ -52,25 +52,27 @@
 //                        .....EVENTS and the keyword this.....
 //=======================================================================================
 //=======================================================================================
+let newColor= "";
 const makeRandColor = () => {
     const r = Math.floor(Math.random() * 255);
     const g = Math.floor(Math.random() * 255);
     const b = Math.floor(Math.random() * 255);
-    return `rgb(${r}, ${g}, ${b})`
+    newColor = `rgb(${r}, ${g}, ${b})`;
+    return newColor;
 }
 const buttons = document.querySelectorAll('button');
 
-// for(let button of buttons){
-//     button.addEventListener('click', () => {
-//     button.style.backgroundColor = makeRandColor();
-//     button.style.color = makeRandColor();
-//     })
-// }
+// // for(let button of buttons){
+// //     button.addEventListener('click', () => {
+// //     button.style.backgroundColor = makeRandColor();
+// //     button.style.color = makeRandColor();
+// //     })
+// // }
 for (let button of buttons) {
     button.addEventListener('click', function handleClick() {
         button.style.backgroundColor = makeRandColor();
         button.style.color = makeRandColor();
-        
+        button.textContent = newColor;
         // Remove the event listener after the first click
         button.removeEventListener('click', handleClick);
     });
