0630e9ce31b05666a28511264b6a494c6e95c09e
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2024-08-28 22:29:09 +0500

    JS single threaded, handling WebAPIS function

--- Async JavaScript Oh Boys/app.js
+++ Async JavaScript Oh Boys/app.js
@@ -1,15 +1,32 @@
-console.log("Hello");
+// console.log("Hello");
 
-let multiply = (x,y) => {
-    return x * x;
-}
+// let multiply = (x,y) => {
+//     return x * x;
+// }
 
-let square = x => {
-    return x*x;
-}
+// let square = x => {
+//     return x*x;
+// }
 
-function isRightTriangle(a,b,c) {
-    return square(a) + square(b) === square(c);
-}
+// function isRightTriangle(a,b,c) {
+//     return square(a) + square(b) === square(c);
+// }
 
-isRightTriangle(3,4,5);
+// isRightTriangle(3,4,5);
+
+// console.log("After")
+
+//=======================================================================================
+//                         ....WEB APIS....
+//=======================================================================================
+console.log("Sending request to server");
+
+setTimeout(() => {
+    console.log("Here is you data from the server");
+},3000);
+console.log("I am at the end of the file");
+
+//=======================================================================================
+//                         ....WEB APIS....
+//=======================================================================================
+
