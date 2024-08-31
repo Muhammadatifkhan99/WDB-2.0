178c7d48d2de6fb7512c833a75e60455e1086552
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2024-08-31 16:20:57 +0500

    make XmlHttpRequest using JavaScript

--- AJAX and API's/app.js
+++ AJAX and API's/app.js
@@ -47,15 +47,38 @@ const data = {
 //=======================================================================================
 //accept ---------> application/json
 
-
-
 //=======================================================================================
 //=======================================================================================
-//                                 HTTP VERBS
+//                                 XHR(xmlhttprequests)
 //=======================================================================================
 //=======================================================================================
 
+//the original way of sending requests from JS
+const req  = new XMLHttpRequest();
 
+req.onload = function() {
+    console.log("Its Loaded");
+    const data = JSON.parse(this.responseText);
+    console.log(data.name, data.height);
+}
+req.onerror = function () {
+    console.log("ERROR!!!!");
+    console.log(this);
+}
+
+req.open("GET", "https://swapi.dev/api/people/1");
+req.send();
+
+const req = new XMLHttpRequest();
+req.onload = function(){
+
+}
+req.onerror = function () {
+    
+}
+
+req.open("GET","url");
+req.send();
 
 //=======================================================================================
 //=======================================================================================
