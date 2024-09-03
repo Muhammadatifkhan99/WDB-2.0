f102c84a58ab20ecefdbc34a5976ce6cad03eb2e
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2024-09-04 00:03:37 +0500

    Refactored the code fot constructor functions

--- Prototypes, classes__OOP/app.js
+++ Prototypes, classes__OOP/app.js
@@ -86,7 +86,15 @@ Color.prototype.rgb = function () {
     return `rgb(${r},${g},${b})`
 };
 
+Color.prototype.hex = function () {
+    const {r, g ,b } = this;
+    return '#'+ ((1 << 24) + (r << 16) + (g << 8) +b).toString(16).slice(1);
+}
 
+Color.prototype.rgba = function (a=1.0) {
+    const {r,g,b} = this;
+    return `rgb(${r},${g},${b},${a})`
+}
 
 
 //=======================================================================================
