4c117a676e52a61872e2e3bcdaba8f4ccbf75b62
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2024-09-03 23:39:15 +0500

    factory functions in JS

--- Prototypes, classes__OOP/app.js
+++ Prototypes, classes__OOP/app.js
@@ -1,30 +1,102 @@
-const arr = [1,2,3,4];
+// const arr = [1,2,3,4];
 
 
-console.log(arr.push(5)); //the push method is not defined on the arr but its defined on prototype
-console.log(arr);
+// console.log(arr.push(5)); //the push method is not defined on the arr but its defined on prototype
+// console.log(arr);
 //any of the method that we used will not be seen
 //the __proto__ object contains all of the objects
 
-arr.includes(2); //this include method is not defined on every single array its defined
+//arr.includes(2); //this include method is not defined on every single array its defined
                 // on the prototype object
                 //prototypes are template objects
 
 
-Array.prototype;
+// Array.prototype;
 
 
 //this is how to define a mehtod on any String Object
-String.prototype.yell = function () {
-    return`OMG!!!!${this.toUpperCase()}!!!!AGAHAGAHAG!`;
-}
+// String.prototype.yell = function () {
+//     return`OMG!!!!${this.toUpperCase()}!!!!AGAHAGAHAG!`;
+// }
 
 //defining methods on Array
 
-Array.prototype.pop =  function () {
-    return `SORRY I WANT THAT ELEMENT!!!I WILL NEVER POP IT`
-}
+// Array.prototype.pop =  function () {
+//     return `SORRY I WANT THAT ELEMENT!!!I WILL NEVER POP IT`
+// }
 
 //the prototype is the actual object where we add the mehtods or the properties
 
 //__proto__ is a reference on the property name
+
+
+//=======================================================================================
+//=======================================================================================
+//                                  Factory Functions
+//=======================================================================================
+//=======================================================================================
+
+// function hex(r,g,b){
+//     return '#' + ((1 << 24) + (r << 16) + (g << 8) +b).toString(16).slice(1);
+// }
+
+// // hex(255,34,22);
+// // '#ff2216'
+
+// function makeColor (r,g,b) {
+//     const color = {};
+//     color.r = r;
+//     color.g = g;
+//     color.b = b;
+//     color.rgb = function () {
+//         const {r, g, b} = this;
+//         return `rgb(${r}, ${g}, ${b})`
+//     };
+//     color.hex = function () {
+//         const {r, g ,b } = this;
+//         return '#'+ ((1 << 24) + (r << 16) + (g << 8) +b).toString(16).slice(1);
+//     }
+//     return color;
+// }
+
+// const firstColor = makeColor(35,255,150);
+// firstColor.hex();
+
+
+
+//=======================================================================================
+//=======================================================================================
+//                                  Constructor Functions
+//=======================================================================================
+//=======================================================================================
+
+
+
+
+
+//=======================================================================================
+//=======================================================================================
+//                                  Factory Functions
+//=======================================================================================
+//=======================================================================================
+
+
+
+//=======================================================================================
+//=======================================================================================
+//                                  Factory Functions
+//=======================================================================================
+//=======================================================================================
+
+
+
+
+
+//=======================================================================================
+//=======================================================================================
+//                                  Factory Functions
+//=======================================================================================
+//=======================================================================================
+
+
+
