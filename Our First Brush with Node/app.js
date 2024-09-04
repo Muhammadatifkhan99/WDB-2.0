af56243eff6f04a061025e80e2050844f53722b1
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2024-09-04 22:45:15 +0500

    The process object in node with argv

--- Our First Brush with Node/app.js
+++ Our First Brush with Node/app.js
@@ -1,3 +1,11 @@
-for(let i=0;i<10;i++){
-    console.log("Hello from the first script");
+// for(let i=0;i<10;i++){
+//     console.log("Hello from the first script");
+
+const args = process.argv.slice(2);
+
+for(let arg of args){
+    console.log(`Hi there, ${arg}`);
 }
+
+
+
