d7143b1bd7bc6000fd63f6d5f71988b2028fbde1
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2024-09-05 13:19:08 +0500

    Requiring a directory to export functions/modules etc

--- Exploring Module_The NPM Universe/app.js
+++ Exploring Module_The NPM Universe/app.js
@@ -1,9 +1,13 @@
 const math = require('./math');
+const cats = require('./Shelter');
+
+
+console.log("REQUIRED AN ENTIRE DIRECTORY !!",cats);
 //we can destructure this as below
 //const {PI, square} = require('./math');
 
 
 
-console.log(math);
-console.log(math.PI);
-console.log(math.square(9));
+// console.log(math);
+// console.log(math.PI);
+// console.log(math.square(9));
