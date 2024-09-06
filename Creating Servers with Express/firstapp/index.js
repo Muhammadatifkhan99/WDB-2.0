a1827eb0937aa158416c831eed39f0647d9be8e2
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2024-09-07 01:12:41 +0500

    requesting with some response to the client...i) JSON and Normal request

--- Creating Servers with Express/firstapp/index.js
+++ Creating Servers with Express/firstapp/index.js
@@ -2,8 +2,20 @@ const express = require("express");
 
 const app = express();
 // console.dir(app);
-app.use(() => {
+
+
+//on every request we have access to two parameters in the use() function 
+app.use((req,res) => {
     console.log("WE GOT A NEW REQUEST")
+    // console.dir(req);
+    //res.send() generate and sends an http response
+    // res.send("HELLO WE GOT YOU REQUEST!! THIS IS A RESPONSE!!")
+    // const color = {
+    //     color: 'red',
+    //     age: '23'
+    // }
+    // res.send(color);
+    res.send("<h1>Hello and Welcome</h1>")
 })
 
 
