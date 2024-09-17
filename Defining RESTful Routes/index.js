83b66cd57a30aef7d2edae950b15e640825246f2
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2024-09-17 23:54:04 +0500

    Parsing the request body

--- Defining RESTful Routes/index.js
+++ Defining RESTful Routes/index.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const app = express();
 
+//app.use code will always run when ever a request is made to this page....the type of request does not matters
+
+//to extract form encoded information we use a middleware 
+app.use(express.urlencoded({extended: true}))
+
+//to extract information from json we need another method that can work with json payloads
+app.use(express.json());
+
+// on the get request we have the req.query because get requests are usally a query strings
 app.get('/tacos', (req,res) => {
     res.send("GET /tacos Request");
 })
-
+//on the post request we have the req.body
 app.post('/tacos', (req,res) => {
-    res.send("POST /tacos Request")
+    console.log(req.body)
+    const {meat, qty} = req.body; 
+    res.send(`OK here are your ${qty} ${meat} tacos`)
 })
 
 app.listen(3000, () => {
