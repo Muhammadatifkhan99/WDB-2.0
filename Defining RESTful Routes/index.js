f1a477da91804ec5165d9f5e058f094c0b65b8b0
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2024-09-18 22:38:56 +0500

    RESTful routes for CRUD Operations

--- Defining RESTful Routes/index.js
+++ Defining RESTful Routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 //app.use code will always run when ever a request is made to this page....the type of request does not matters
 
@@ -8,6 +9,31 @@ app.use(express.urlencoded({extended: true}))
 
 //to extract information from json we need another method that can work with json payloads
 app.use(express.json());
+
+//set to use the view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname,'views'));
+
+//array to hold comments to be used to demonstrate routes
+
+const comments = [
+    {
+        username: 'Todd',
+        comment : 'lol, that is so funny'
+    },
+    {
+        username: 'Skyler',
+        comment: 'I like to go birdwatching with my dog'
+    },
+    {
+        username: 'Sk8erBio',
+        comment: 'Plz delete your account Todd'
+    },
+    {
+        username: 'onlysaywoofwoof',
+        comment: 'woof woof woof'
+    }
+]
 
 // on the get request we have the req.query because get requests are usally a query strings
 app.get('/tacos', (req,res) => {
