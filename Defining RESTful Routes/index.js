7b3e3b42e26f99d1cfe9f0415860dda4dfcfe252
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2024-09-19 11:31:26 +0500

    express redirects

--- Defining RESTful Routes/index.js
+++ Defining RESTful Routes/index.js
@@ -40,6 +40,18 @@ app.get('/comments', (req,res) => {
     res.render('comments/index', {comments});
 })
 
+app.get('/comments/new',(req,res) => {
+    res.render('comments/new');
+})
+
+app.post('/comments', (req,res) => {
+    // console.log(req.body);
+    const {username, comment} = req.body;
+    comments.push({username, comment});
+    // res.send("IT WORKED")
+    res.redirect('/comments')
+})
+
 // on the get request we have the req.query because get requests are usally a query strings
 app.get('/tacos', (req,res) => {
     res.send("GET /tacos Request");
