6a26afeb6a19abef56335b098148fdcbb6111010
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2024-10-02 15:40:35 +0500

    Connecting to a mongodb through mongoose

---
+++ Connecting Mongoose to Mongo/index.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb://localhost:27017/moviesApp")
+    .then(() => {
+        console.log("CONNECTION OPEN");
+    })
+    .catch(err => {
+        console.log("ERROR");
+        console.log(err);
+    })
+
+
+// const db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'connection error:'));
+// db.once('open', function() {
+//     console.log("CONNECTION OPEN")
+// });
