8110bc3b119a0873d50dbe9497f3657ebc037ee4
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2024-10-02 23:37:13 +0500

    The Mongoose model

--- Connecting Mongoose to Mongo/index.js
+++ Connecting Mongoose to Mongo/index.js
@@ -15,3 +15,19 @@ mongoose.connect("mongodb://localhost:27017/moviesApp")
 // db.once('open', function() {
 //     console.log("CONNECTION OPEN")
 // });
+
+
+const movieSchema = new mongoose.Schema({
+    title : String,
+    year: Number,
+    score: Number,
+    rating: String
+})
+
+//This will gives us a class
+const Movie = mongoose.model('Movie', movieSchema);
+
+//This will make a new instance of the Movie class
+const Amadeus = new Movie ({title: 'amadeus', year: 1986, score: 9.2, rating: 'R'});
+
+
