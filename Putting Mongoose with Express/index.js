9cf845e66f221e266d63ca892386c91afeb671f2
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2024-10-25 17:54:48 +0500

    product index page

--- Putting Mongoose with Express/index.js
+++ Putting Mongoose with Express/index.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
 
-const product = require("./models/product");
+const Product = require("./models/product");
 
 mongoose.connect("mongodb://localhost:27017/farmStand")
     .then(() => {
@@ -23,17 +23,14 @@ app.set("view engine", "ejs");
 
 //adding a basic route
 
-app.get("/dog", (req,res) => {
-    res.send("WOOF");
+app.get("/products", async (req,res) => {
+    const products = await Product.find({})
+    // console.log(products);
+    res.render("products/index", { products });
 })
 
 
 
-
-
-
-
-
-app.listen(3000, () => {
-    console.log("APP IS LISTENING ON PORT 3000!");
+app.listen(4000, () => {
+    console.log("APP IS LISTENING ON PORT 4000!");
 })
