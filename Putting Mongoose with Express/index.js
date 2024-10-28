b017b8dc3bb5ada36f46d27aecc1ce45b6afcd8c
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2024-10-28 12:06:02 +0500

    creating new product

--- Putting Mongoose with Express/index.js
+++ Putting Mongoose with Express/index.js
@@ -21,12 +21,33 @@ mongoose.connect("mongodb://localhost:27017/farmStand")
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+//express middleware
+app.use(express.urlencoded({extended:true}))
+
 //adding a basic route
 
 app.get("/products", async (req,res) => {
     const products = await Product.find({})
     // console.log(products);
     res.render("products/index", { products });
+})
+
+//creating the product route..........................
+app.get("/products/new", async (req,res) => {
+    res.render("products/new");
+})
+
+//route for submission of the form i.e is to the /products
+app.post("/products", async(req,res) => {
+    //in post requests when information is required from the post request body, we do not have access to it 
+    //right away,,,its undefined if access,,, use express middle wares to deal with this....
+    //app.use(express.urlendcoded({extended:true}))
+    const newProduct = new Product(req.body);
+    await newProduct.save();
+    console.log(newProduct);
+    //redirecting to the show page for each product
+    // res.send("making your product");
+    res.redirect(`/products/${ newProduct._id }`)
 })
 
 app.get("/products/:id", async (req, res) => {
