205027868f4e4a21e2f04090ce069a97211fd08a
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2024-10-28 23:02:11 +0500

    update in crud, along with the validators

--- Putting Mongoose with Express/index.js
+++ Putting Mongoose with Express/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
+const methodoverride = require("method-override");
 
 const Product = require("./models/product");
 
@@ -23,16 +24,16 @@ app.set("view engine", "ejs");
 
 //express middleware
 app.use(express.urlencoded({extended:true}))
+app.use(methodoverride("_method"));
 
 //adding a basic route
-
 app.get("/products", async (req,res) => {
     const products = await Product.find({})
     // console.log(products);
     res.render("products/index", { products });
 })
 
-//creating the product route..........................
+//creating the new product route..........................
 app.get("/products/new", async (req,res) => {
     res.render("products/new");
 })
@@ -56,6 +57,19 @@ app.get("/products/:id", async (req, res) => {
     // console.log(product);
     // res.send("Details Page");
     res.render("products/show", { product });
+})
+
+app.get("/products/:id/edit", async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findById(id)
+    res.render("products/edit", { product });
+})
+
+app.put("/products/:id", async (req,res) => {
+    // console.log(req.body);
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body, {runValidators: true, new:true});
+    res.redirect(`/products/${product._id}`);
 })
 
 app.listen(4000, () => {
