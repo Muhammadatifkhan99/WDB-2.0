051c7850e3a6eb69592e105cef73e1b2dc3a0f9f
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2024-11-10 22:09:22 +0500

    more practice on middleware

--- Middleware-The Key to Express/index.js
+++ Middleware-The Key to Express/index.js
@@ -11,30 +11,46 @@ const morgan = require("morgan");
 // app.use(morgan("common"));
 // app.use(morgan("dev"));
 
+app.use(morgan("tiny"));
+app.use((req,res, next) => {
+    //making every request a GET request
+    // req.method  = "GET";
+    req.requestTime = Date.now();
+    console.log(req.method, req.path);
+    next();
+})
 
-app.use((req,res, next) => {
-    // res.send("HIJACKED BY GOVERNMENT");
-    console.log("This is the first middleware");
-    return next();
-    //code after this will execute
-    console.log("This is the code after the next()");
-});
-app.use((req,res, next) => {
-    // res.send("HIJACKED BY GOVERNMENT");
-    console.log("This is the second middleware");
-    return next();
-});
-app.use((req,res, next) => {
-    // res.send("HIJACKED BY GOVERNMENT");
-    console.log("This is the third middleware");
-    return next();
-});
+
+
+
+
+
+
+// app.use((req,res, next) => {
+//     // res.send("HIJACKED BY GOVERNMENT");
+//     console.log("This is the first middleware");
+//     return next();
+//     //code after this will execute
+//     console.log("This is the code after the next()");
+// });
+// app.use((req,res, next) => {
+//     // res.send("HIJACKED BY GOVERNMENT");
+//     console.log("This is the second middleware");
+//     return next();
+// });
+// app.use((req,res, next) => {
+//     // res.send("HIJACKED BY GOVERNMENT");
+//     console.log("This is the third middleware");
+//     return next();
+// });
 
 app.get("/", (req,res) => {
+    console.log(`REQUEST DATE: ${req.requestTime}`);
     res.send("HOME PAGE");
 })
 
 app.get("/dogs", (req,res) => {
+    console.log(`REQUEST DATE: ${req.requestTime}`);
     res.send("WOOF WOOF");
 })
 
