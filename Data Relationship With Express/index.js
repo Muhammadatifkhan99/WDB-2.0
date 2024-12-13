42a9d1db04ec73df13c77e8def83d4469bf1b56f
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2024-12-13 08:06:00 -0800

    creating products and integrating it into farms

--- Data Relationship With Express/index.js
+++ Data Relationship With Express/index.js
@@ -49,6 +49,24 @@ app.get("/farms/:id", async(req,res) => {
     res.render("farms/show", { farms });
 })
 
+app.get("/farms/:id/products/new", (req,res) => {
+    const { id } = req.params;
+    res.render("products/new", { categories, id});
+})
+
+app.post("/farms/:id/products",async (req,res) => {
+    const { id } = req.params;
+    const farm = await Farm.findById(id);
+    const { name, city , email } = req.body;
+    const product = new Product({name,city,email});
+    farm.product.push(product);
+    product.farm = farm;
+    await farm.save();
+    await product.save();
+    res.send(farm);
+})
+
+
 app.post("/farms", async (req,res) => {
     const farm = new Farm(req.body);
     await farm.save();
