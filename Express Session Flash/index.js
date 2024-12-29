03f8318706139d3096ebf03abd83c43b5245801b
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2024-12-29 14:32:14 +0500

    more on express sessions

--- Express Session Flash/index.js
+++ Express Session Flash/index.js
@@ -16,6 +16,17 @@ app.get("/viewcount",(req,res) => {
     res.send(`You have viewed this page ${req.session.count} times`);
 })
 
+app.get("/register",(req,res) => {
+    const { username = "Anonymous" } = req.query;
+    req.session.username = username;
+    res.redirect("/greet");
+})
+
+app.get("/greet",(req,res) => {
+    const { username } = req.session;
+    res.send(`Welcome back, ${username}`);
+})
+
 app.listen("3000",()=>{
     console.log("SERVING ON PORT 3000");
 })
