76a71bb9b29066e0b480c50df602edaf692769f6
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2025-01-04 21:14:28 +0500

    registering a user and hashing the password using bcrypt

--- Authentications From Scratch/AuthDemo/index.js
+++ Authentications From Scratch/AuthDemo/index.js
@@ -1,14 +1,46 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const User = require("./models/user");
+const bcrypt = require("bcrypt");
 
-const user = require("./models/user");
+
+
+
+//connection to the database
+mongoose.connect("mongodb://localhost:27017/authDemo");
+
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database Connected");
+})
 
 app.set("view engine", "ejs");
 app.set("views", "views");
 
+app.use(express.urlencoded({extended:true}))
+
+app.get("/",(req,res) => {
+    res.send("This is the home page");
+})
+
 
 app.get("/register",(req,res) => {
     res.render("register");
+})
+
+app.post("/register", async (req,res) => {
+    const { username , password } = req.body;
+    const hash = await bcrypt.hash(password,12);
+    const user = new User ({
+        username,
+        password: hash
+    })
+    await user.save();
+    res.redirect("/")
+    // res.send(req.body);
 })
 
 
